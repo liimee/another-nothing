5511b486692c97a5bf0735edc3a57b63930e59fe
ta <alt3753.7@gmail.com>
2021-11-25 09:46:27 +0700

    idk

--- f/main.js
+++ f/main.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState, Component } from 'react';
 import ReactDOM from 'react-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleUp } from '@fortawesome/free-solid-svg-icons'
 
-ReactDOM.render(<App />, document.querySelector('#app'))
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {windows: ['Welcome']}
+  }
 
-function App() {
-  return <><div className="desktop"><Bar /></div></>
+  click() {
+    var d = s;
+    d.push('welcome');
+    setS(d)
+  }
+
+  render() {
+    return (
+      <><div className="desktop">
+      {this.state.windows.map(() => {return <Window app="Welcome" />})}
+      <Bar />
+      </div></>
+    )
+  }
 }
 
 function Bar() {
@@ -14,3 +30,9 @@ function Bar() {
   //TODO: connected/disconnected status?
   return <div className="bar">Apps <FontAwesomeIcon icon={faAngleUp} /></div>
 }
+
+function Window({app}) {
+  return <div className="window"><iframe src={"/apps/"+app} /></div>
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'))
