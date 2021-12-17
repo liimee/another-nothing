806e575616b513525e0dff8b7b939cc6e76a3412
ta <alt3753.7@gmail.com>
2021-12-17 15:14:55 +0700

    e

--- f/main.tsx
+++ f/main.tsx
@@ -81,7 +81,7 @@ class Bar extends Component<{openthing: Function, w: JSX.Element[]}, {apps: {}, 
       <>
       <div className="bar"><Tippy placement='top-start' content={this.state.c ? 'Server-sent events should be received properly' : 'Server-sent events may not be received'}><span style={{display: 'inline-block', backgroundColor: this.state.c ? 'var(--color4)' : 'var(--color2)', padding: '1em', color: '#fff', borderBottomLeftRadius: '6px', borderTopLeftRadius: '6px'}}><FontAwesomeIcon icon={faWifi} /></span></Tippy><span>{this.props.w}</span><Tippy content={
         <><div><b>another nothing <Tippy placement='top-end' content='sign out'><a href='/logout' style={{float: 'right'}}><FontAwesomeIcon icon={faSignOutAlt} /></a></Tippy></b></div><h2 style={{marginTop: 0.5}}>apps</h2> {Object.keys(this.state.apps).map((v) => <span onClick={() => this.openApp(v, this.state.apps[v].name)}><img className="icon" src={`/apps/${v}/icon.svg`} alt={`${this.state.apps[v].name} icon`} /> {this.state.apps[v].name}</span>)}</>
-      } arrow={false} theme="light" interactive={true}><span onClick={() => this.toggle()} style={{display: 'inline-block', padding: '1em'}}><FontAwesomeIcon icon={faArrowUp} /></span></Tippy></div>
+      } arrow={false} theme="light" interactive={true}><span style={{display: 'inline-block', padding: '1em'}}><FontAwesomeIcon icon={faArrowUp} /></span></Tippy></div>
       </>
     )
   }
