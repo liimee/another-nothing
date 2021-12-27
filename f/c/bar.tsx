17c31a22b9e3e034f036759ae0a5fe0c979ff399
ta <alt3753.7@gmail.com>
2021-12-27 14:24:15 +0700

    e

--- f/c/bar.tsx
+++ f/c/bar.tsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Tippy from '@tippyjs/react';
 import evs from '../evs';
-import { faArrowUp, faSignOutAlt, faWifi } from '@fortawesome/free-solid-svg-icons';
+import { faArrowUp, faSignOutAlt, faWifi, faCogs } from '@fortawesome/free-solid-svg-icons';
 
 export default class Bar extends Component<{openthing: Function, w: JSX.Element[]}, {apps: {}, c: Boolean}> {
   //favorites [apps]?
@@ -43,7 +43,7 @@ export default class Bar extends Component<{openthing: Function, w: JSX.Element[
     return (
       <>
       <div className="bar a-f"><Tippy placement='top-start' content={this.state.c ? 'Server-sent events should be received properly' : 'Server-sent events may not be received'}><span style={{display: 'inline-block', backgroundColor: this.state.c ? 'var(--color4)' : 'var(--color2)', padding: '1em', color: '#fff', borderBottomLeftRadius: '6px', borderTopLeftRadius: '6px'}}><FontAwesomeIcon icon={faWifi} /></span></Tippy><span>{this.props.w}</span><Tippy content={
-        <><div><b>another nothing <Tippy placement='top-end' content='sign out'><a href='/logout' style={{float: 'right'}}><FontAwesomeIcon icon={faSignOutAlt} /></a></Tippy></b></div><h2 style={{marginTop: 0.5}}>apps</h2> {Object.keys(this.state.apps).map((v) => <span onClick={() => this.openApp(v, this.state.apps[v].name)}><img className="icon" src={`/apps/${v}/icon.svg`} alt={`${this.state.apps[v].name} icon`} /> {this.state.apps[v].name}</span>)}</>
+        <><div><b>another nothing <span style={{float: 'right'}}><Tippy content='settings'><a href='/settings'><FontAwesomeIcon icon={faCogs} /></a></Tippy><Tippy placement='top-end' content='sign out'><a href='/logout'><FontAwesomeIcon icon={faSignOutAlt} /></a></Tippy></span></b></div><h2 style={{marginTop: 0.5}}>apps</h2> {Object.keys(this.state.apps).map((v) => <span className="api" onClick={() => this.openApp(v, this.state.apps[v].name)}><img className="icon" src={`/apps/${v}/icon.svg`} alt={`${this.state.apps[v].name} icon`} /> {this.state.apps[v].name}</span>)}</>
       } arrow={false} theme="light" interactive={true}><span style={{display: 'inline-block', padding: '1em'}}><FontAwesomeIcon icon={faArrowUp} /></span></Tippy></div>
       </>
     )
