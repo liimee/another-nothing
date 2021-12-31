bdded66341b417915422800dec9542039bc4def2
ta <alt3753.7@gmail.com>
2021-12-31 13:45:00 +0700

    e

--- main.rb
+++ main.rb
@@ -211,7 +211,7 @@ post '/settings' do
   #kinda dumb but ok
   s = URI(request.referrer)
   u = checklogin(request)["user"]
-  halt 500, 'no.' unless s.path == '/settings' && !request.xhr? && request.env["HTTP_SEC_FETCH_DEST"] != 'iframe'
+  halt 500, 'no.' unless s.host == HHH && s.path == '/settings' && !request.xhr? && request.env["HTTP_SEC_FETCH_DEST"] != 'iframe'
 
   if BCrypt::Password.new($users.first(:username => u)[:password]) == params["pass"]
     $users.where(:username => u).update(params.reject{|k| k.start_with?('app_')||k == 'pass'||k == 'users'||k.start_with?('uninstall_')})
